bad651192a57b4a4fafb2c4e813a9b8bedec99d6
Kwakye William Biney <liambiney@gmail.com>
2025-12-10 00:11:49 -0600

    initial commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+const sideNav = document.getElementById('side-nav');
+const sideNavBackdrop = document.querySelector('.side-nav__backdrop');
+const menuBtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('close-btn');
+
+function toggleSideNav(){
+    sideNav?.style.setProperty('transform', 'translateX(0)');
+    sideNavBackdrop?.style.setProperty('display', 'block');
+}
+
+function collapseNav(){
+    sideNav?.style.setProperty('transform', 'translateX(-320px)');
+    sideNavBackdrop?.style.setProperty('display', 'none');
+}
+
+menuBtn?.addEventListener('click', toggleSideNav);
+closeBtn?.addEventListener('click', collapseNav);
