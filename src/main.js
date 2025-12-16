9e3cce1687e186ee36a1220c733cc4953ee7689b
Kwakye William Biney <liambiney@gmail.com>
2025-12-16 11:09:43 -0600

    added target section and its correspoding css styling. Also added intersection observer logic for the target section card higlighting.

--- src/main.js
+++ src/main.js
@@ -21,3 +21,25 @@ sideNavBackdrop?.addEventListener('click', () =>{
         collapseNav();
     }
 })
+
+const cards = document.querySelectorAll('.built-for__card');
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                cards.forEach(card =>
+                    card.classList.remove('is-active')
+                );
+                entry.target.classList.add('is-active');
+            }
+        });
+    },
+    {
+        root: null,
+        threshold: 0,
+        rootMargin: '-50% 0px -50% 0px'
+    }
+);
+
+cards.forEach(card => observer.observe(card));
