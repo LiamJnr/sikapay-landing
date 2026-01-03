e6de21571287fa7633c5dbeb89f8d5011c4ccd44
Gifty Kwakye <Biney William>
2026-01-03 23:55:50 +0000

    updated the illustrations for the security section using canvas. Added new securityCanvas.js file and updated corresponding securitySection.css styles

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { cardColorSwap } from "./components/cardColorSwap.js";
+import { digitalFootprintCanvas, encryptionCanvas } from "./components/securityCanvas.js";
 import { revealIcon } from "./components/featureIconReveal.js";
 import { sidebarToggler } from "./components/sidebarToggler.js";
 import { scalabilityTextReveal, securityTextReveal } from "./components/textReveal.js";
@@ -20,4 +21,9 @@ window.addEventListener('DOMContentLoaded', () => {
     // Reveal icon logic
     revealIcon();
 
+    //initialize encrytion Canvas
+    encryptionCanvas();
+
+    // initialize digital footprint canvas
+    digitalFootprintCanvas();
 })
